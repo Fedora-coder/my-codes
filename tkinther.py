602afaf305d6c9429ed7d5c5ed515f956c8bd9a5
Fedora-coder <fedoranduka262@gmail.com>
2024-12-28 16:25:20 +0100

    my python codes

---
+++ tkinther.py
@@ -0,0 +1,16 @@
+#sums up the values
+
+#4= 4+3+2+1=10
+
+#first option
+
+def first(n): #constant space complexity
+
+    return n*(n+1)/2 #(4*5) = 20/2=10
+
+print('First output')
+
+print(first(4))
+
+
+
